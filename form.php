48b7d1835d495065e84758d9aa43b1875ca5bfec
ericfesta <eric@allfesta.com>
2015-08-22 21:02:49 +0900

    21:02

---
+++ form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+<form class="" id="" action="" accept-charset="UTF-8" method="post">
+  <input name="utf8" type="hidden" value="✓"><input type="" name="" value="">
+
+  <div class="form">
+    <label for="pot_email">Email</label><br>
+    <textarea name="[email]" id="email"></textarea>
+  </div>
+  <div class="form">
+    <label for="pot_password">Password</label><br>
+    <textarea name="[password]" id="password"></textarea>
+  </div>
+  <div class="actions">
+    <input type="submit" name="commit" value="">
+  </div>
+</form>
+</body>
+</html>
